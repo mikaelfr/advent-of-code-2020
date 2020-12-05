65e1776f6764276373be19f66c6817e7992cd77e
LazyKernel <miksu.fredriksson@hotmail.com>
2020-12-06 01:00:09 +0200

    Add day 5, update caching in fetch

---
+++ day5.py
@@ -0,0 +1,40 @@
+from functools import reduce
+from operator import itemgetter
+from fetch import AdventCalendar
+
+class PassParser:
+
+    def __init__(self):
+        boarding_passes = [PassParser.replace_to_bin(row) for row in AdventCalendar(5).get_daily_input_data()]
+
+        self.passes = sorted([
+            {
+                'row': int(row[:7], 2),
+                'col': int(row[7:], 2), 
+                'sid': int(row[:7], 2) * 8 + int(row[7:], 2)
+            }
+            for row in boarding_passes
+        ], key=itemgetter('sid'))
+
+    def get_adjacent_pairs(self):
+        i = 0
+        for _ in range(len(self.passes) - 1):
+            i += 1
+            yield (self.passes[i-1], self.passes[i])
+
+    @staticmethod
+    def replace_to_bin(row):
+        return PassParser.bulk_replace(row, {'B': '1', 'F': '0', 'R': '1', 'L': '0'})
+
+    @staticmethod
+    def bulk_replace(in_str, mapping):
+        for key in mapping:
+            in_str = in_str.replace(key, mapping[key])
+        return in_str
+        
+
+result1 = max([bpass['sid'] for bpass in PassParser().passes])
+result2 = [pair[1]['sid']-1 for pair in PassParser().get_adjacent_pairs() if pair[0]['sid'] + 2 == pair[1]['sid']][0]
+
+print(result1)
+print(result2)
--- fetch.py
+++ fetch.py
@@ -5,18 +5,20 @@ import browser_cookie3 as browsercookie
 class AdventCalendar(object):
     """ Class to authenticate and get input data from AoC
     """
+    cache = {}
+
     def __init__(self, day):
         """ Initialization
         """
-        self.cached_response = None
+        self.day = str(day)
         self.url = "https://adventofcode.com/2020/day/" + str(day) + "/input"
 
     def get_daily_input_data(self):
         """ Get session_id from cookie for AoC site"""
-        if self.cached_response:
-            return self.cached_response
+        if self.day in AdventCalendar.cache:
+            return AdventCalendar.cache[self.day].copy()
         
         cj = browsercookie.firefox()
         response = requests.get(self.url, cookies=cj)
-        self.cached_response = response.text.split('\n')[:-1]
+        AdventCalendar.cache[self.day] = response.text.split('\n')[:-1]
         return response.text.split('\n')[:-1]
